7db845b8a3f9ac20364272580f969e6c78964ff3
tayfurunal <mtayfurunal@gmail.com>
2020-07-01 17:07:11 +0300

    old articles add

--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -1,39 +1,39 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import { Heading } from '@theme-ui/components'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import React from 'react'
-import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout'
-import ItemTags from '@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags'
-import SEO from './seo'
+import { jsx } from 'theme-ui';
+import { Heading } from '@theme-ui/components';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import React from 'react';
+import Layout from '@lekoarts/gatsby-theme-minimal-blog/src/components/layout';
+import ItemTags from '@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags';
+import SEO from './seo';
 
 type PostProps = {
   data: {
     post: {
-      slug: string
-      title: string
-      date: string
+      slug: string;
+      title: string;
+      date: string;
       tags?: {
-        name: string
-        slug: string
-      }[]
-      description?: string
-      body: string
-      excerpt: string
-      timeToRead: number
+        name: string;
+        slug: string;
+      }[];
+      description?: string;
+      body: string;
+      excerpt: string;
+      timeToRead: number;
       banner?: {
         childImageSharp: {
           resize: {
-            src: string
-          }
-        }
-      }
-    }
-  }
-}
+            src: string;
+          };
+        };
+      };
+    };
+  };
+};
 
-const px = [`32px`, `16px`, `8px`, `4px`]
-const shadow = px.map(v => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
+const px = [`32px`, `16px`, `8px`, `4px`];
+const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`);
 
 const Post = ({ data: { post } }: PostProps) => (
   <Layout>
@@ -45,7 +45,14 @@ const Post = ({ data: { post } }: PostProps) => (
     <Heading variant='h2' as='h2'>
       {post.title}
     </Heading>
-    <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
+    <p
+      sx={{
+        color: `secondary`,
+        mt: 3,
+        a: { color: `secondary` },
+        fontSize: [1, 1, 2],
+      }}
+    >
       <time>{post.date}</time>
       {post.tags && (
         <React.Fragment>
@@ -56,10 +63,20 @@ const Post = ({ data: { post } }: PostProps) => (
       {` — `}
       <span>{post.timeToRead} min read</span>
     </p>
-    <section sx={{ my: 5, h2: { margin: '50px 0', fontSize: '2rem' }, h3: { margin: '40px 0', fontSize: '1.5rem' }, '.gatsby-resp-image-wrapper': { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
+    <section
+      sx={{
+        my: 5,
+        h2: { margin: '50px 0', fontSize: '2rem' },
+        h3: { margin: '40px 0', fontSize: '1.5rem' },
+        '.gatsby-resp-image-wrapper': {
+          my: [4, 4, 5],
+          boxShadow: shadow.join(`, `),
+        },
+      }}
+    >
       <MDXRenderer>{post.body}</MDXRenderer>
     </section>
   </Layout>
-)
+);
 
-export default Post
+export default Post;
