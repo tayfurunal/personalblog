04d46a248dee1c2b9d6fa74029e7f18a024957e2
tayfurunal <mtayfurunal@gmail.com>
2020-07-01 17:41:23 +0300

    blog arranged

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,24 @@
 module.exports = {
   siteMetadata: {
-    siteTitle: `Dale Blackburn`,
-    siteTitleAlt: `Dale Blackburn - Full Stack Developer | Brighton, UK`,
-    siteUrl: `https://dakebl.co.uk`,
-    siteDescription: `Dale Blackburn | Full Stack Developer based in Brighton, UK.`,
-    siteLanguage: `en`,
-    author: `@dakebl`,
+    siteTitle: `Tayfur Ünal`,
+    siteTitleAlt: `Tayfur Ünal- Software Developer`,
+    siteUrl: `https://tayfurunal.com`,
+    siteDescription: `Tayfur | Software Developer in Istanbul`,
+    siteLanguage: `tr`,
+    author: `@tayfurunal`,
     externalLinks: [
       {
         name: `Twitter`,
-        url: `https://twitter.com/dakebl`
+        url: `https://twitter.com/tayfurunal`,
       },
       {
         name: `GitHub`,
-        url: `https://www.github.com/dakebl/`
-      }
+        url: `https://www.github.com/tayfurunal/`,
+      },
+      {
+        name: `LinkedIn`,
+        url: `https://www.linkedin.com/in/tayfurunal/`,
+      },
     ],
     navigation: [
       {
@@ -36,16 +40,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Dale Blackburn`,
-        short_name: `dakebl`,
+        name: `Tayfur Ünal`,
+        short_name: `tayfurunal`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#424242`,
         display: `minimal-ui`,
-        icon: `static/favicon-32x32.png`
+        icon: `static/favicon-32x32.png`,
       },
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
   ],
-}
+};
